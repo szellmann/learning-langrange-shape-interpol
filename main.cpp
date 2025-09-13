eb692e0ff80e361700a1d023b6feab2c56e4c38d
Stefan Zellmann <info@szellmann.de>
2025-09-13 05:21:51 +0200

    1D DeCasteljau

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 #include "math.h"
 
 using float3 = math::vec3f;
+using float2 = math::vec2f;
 using interval1 = math::interval1f;
 using interval3 = math::interval3f;
 
@@ -113,6 +114,72 @@ next:
   return false;
 }
 
+// https://www.uni-ulm.de/fileadmin/website_uni_ulm/iui.inst.100/institut/Papers/PrecisionRayTracing.pdf
+template <typename V, int Deg>
+struct BezierCurve
+{
+  enum { D=Deg, };
+  V cp[D];
+
+  void subdivide(BezierCurve &a, BezierCurve &b) const {
+    V dst[D*2];
+    V tmp[D*2];
+
+    memcpy(dst,cp,sizeof(cp));
+
+    int n=D;
+    for (int n=D; n<D*2; ++n) {
+      memcpy(tmp,dst,sizeof(dst));
+      int ptr=0;
+      dst[ptr++] = tmp[0];
+      for (int j=0; j<n-1; ++j) {
+        V v1 = tmp[j];
+        V v2 = tmp[j+1];
+        dst[ptr++] = v1*0.5f+v2*0.5f;
+      }
+      dst[ptr++] = tmp[n-1];
+    }
+
+    memcpy(a.cp,&dst[0],sizeof(a.cp));
+    memcpy(b.cp,&dst[D],sizeof(b.cp));
+  }
+};
+
+template <typename V, int Deg>
+struct BezierQuad
+{
+  enum { D=Deg, };
+  BezierCurve cv[D];
+
+  // TODO:
+  void subdivide(BezierQuad &a, BezierQuad &b) const {
+    // dim-1
+    for (int i=0; i<D; ++i) {
+      BezierCurve ca,cb;
+      cv[i].subdivide(ca,cb);
+      a.cv[i] = ca;
+      b.cv[i] = cb;
+    }
+    // dim-2
+    for (int i=0; i<D; ++i) {
+      BezierCurve c;
+      for (int j=0; j<D; ++j) {
+        c[j] = a.cv[j].cp[i];
+      }
+    }
+  }
+};
+
+template <typename V>
+struct BezierHex
+{
+  static constexpr int D=4;
+  V cp[D][D][D];
+
+  V eval(float x, float y, float z) const {
+  }
+};
+
 int main() {
   LagrangeHex<float3> hex;
   int order=hex.D;
@@ -120,6 +187,25 @@ int main() {
   memcpy(hex.value,data.data(),sizeof(data[0])*data.size());
 
   std::cout << sample(hex,float3(1.f,1.f,1.f)) << '\n';
+
+  BezierCurve<float2,4> bc;
+  bc.cp[0] = float2(0.0f,0.0f);
+  bc.cp[1] = float2(1.0f,1.0f);
+  bc.cp[2] = float2(2.0f,1.0f);
+  bc.cp[3] = float2(3.0f,0.0f);
+
+  BezierCurve<float2,4> a, b;
+  bc.subdivide(a,b);
+
+  for (int i=0; i<a.D; ++i) {
+    std::cout << a.cp[i] << ' ';
+  }
+  std::cout << '\n';
+
+  for (int i=0; i<b.D; ++i) {
+    std::cout << b.cp[i] << ' ';
+  }
+  std::cout << '\n';
 }
 
 
